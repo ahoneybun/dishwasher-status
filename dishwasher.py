027b7c9fb46de83a9e1f505abe7358bb34a49c3d
Aaron Honeycutt <aaron@system76.com>
2020-11-29 18:52:13 -0700

    revert some changes

--- dishwasher.py
+++ dishwasher.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 import sys
-from PyQt5.QtGui     import QFont
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QToolTip, QWidget, QApplication
+from PyQt5.QtGui     import *
+from PyQt5.QtWidgets import *
 from PyQt5.QtCore    import *
 
 
@@ -19,17 +19,15 @@ class Dishwasher(QMainWindow):
 
         btn1 = QPushButton("Clean", self)
         btn1.setToolTip('Press this button to set status to clean')
-        btn1.resize(btn1.sizeHint())
         btn1.move(30, 50)
 
         btn2 = QPushButton("Dirty", self)
         btn2.setToolTip('Press this button to set status to dirty')
-        btn2.resize(btn2.sizeHint())
         btn2.move(150, 50)
       
         btn1.clicked.connect(self.buttonClicked)            
         btn2.clicked.connect(self.buttonClicked)
-        
+
         self.statusBar()
 
         self.showMaximized()
